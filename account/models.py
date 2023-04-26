d5a06f9ed52bc9b399bef4182f1d9380e60e676a
asylbekovatilee <asylbekovanasyikat@gmail.com>
2023-04-26 12:06:13 +0600

    crud product and user

--- account/models.py
+++ account/models.py
@@ -1,6 +1,8 @@
+
 from django.db import models
-from django.contrib.auth.models import AbstractUser, UserManager
+from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.base_user import BaseUserManager
+
 
 class UserManager(BaseUserManager):
     use_in_migrations = True
@@ -8,33 +10,34 @@ class UserManager(BaseUserManager):
     def create_user(self, email, password, phone, **kwargs):
         if not email:
             raise ValueError("Email is required")
-        kwargs["is_stuff"] = True
-        kwargs["is_superuser"] = True
-        kwargs["is_active"] = True
-        email = self.normalize_email(email) # порядок почты normalize email
+        email = self.normalize_email(email)
         user = self.model(email=email, phone=phone, **kwargs)
-        #self.model = User
-        user.set_password(password) # хеширование пароля
-        user.save(using=self._db) # сохряняем юзера в бд
+        # self.model = User
+        user.set_password(password)  # хеширование пароля
+        user.save(using=self._db)  # созраняем юзера в бд
         return user
 
     def create_superuser(self, email, password, phone, **kwargs):
         if not email:
             raise ValueError("Email is required")
-        email = self.normalize_email(email) # порядок почты normalize email
+        kwargs["is_staff"] = True  # даем права суперадмина
+        kwargs["is_superuser"] = True
+        kwargs["is_active"] = True
+        email = self.normalize_email(email)
         user = self.model(email=email, phone=phone, **kwargs)
-        #self.model = User
-        user.set_password(password) # хеширование пароля
-        user.save(using=self._db) # сохряняем юзера в бд
+        # self.model = User
+        user.set_password(password)  # хеширование пароля
+        user.save(using=self._db)  # созраняем юзера в бд
         return user
 
 
 class User(AbstractUser):
-    username = None # убираем юзернейм из полей
+    username = None  # убираем username из полей
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=50)
     bio = models.TextField()
 
-    USERNAME_FIELD = 'email' # указываем какое поле использовать при логине
+    USERNAME_FIELD = 'email'  # указываем какое поле использовать при логине
     REQUIRED_FIELDS = ['phone']
-    objects = UserManager() # указываем нового юзера
+
+    objects = UserManager()  # указываем нового менеджера
--- account/views.py
+++ account/views.py
@@ -1,9 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from drf_yasg.utils import swagger_auto_schema
 
 from .serializers import RegisterUserSerializers
 
 class RegisterUserView(APIView):
+    @swagger_auto_schema(request_body=RegisterUserSerializers())
     def post(self, request):
         serializer = RegisterUserSerializers(data=request.data)
         serializer.is_valid(raise_exception=True)
