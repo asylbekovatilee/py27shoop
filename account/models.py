0140b411b38f32b6315c3afec16a7ef9a6b48790
asylbekovatilee <asylbekovanasyikat@gmail.com>
2023-05-12 11:20:57 +0600

    shop

--- account/models.py
+++ account/models.py
@@ -36,16 +36,13 @@ class UserManager(BaseUserManager):
         return user
 
 
-def create_superuser(self, email, password, phone, **kwargs):
-        if not email:
-            raise ValueError("Email is required")
-        kwargs["is_staff"] = True  # даем права суперадмина
-        kwargs["is_superuser"] = True
-        kwargs["is_active"] = True
-        email = self.normalize_email(email)
-        user = self.model(email=email, phone=phone, **kwargs)
-        # self.model = User
-        user.set_password(password)  # хеширование пароля
-        #отправляем на почту
-        user.save(using=self._db)  # созраняем юзера в бд
-        return user
+class User(AbstractUser):
+    username = None 
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=50)
+    bio = models.TextField()
+
+    USERNAME_FIELD = 'email'  
+    REQUIRED_FIELDS = ['phone']
+
+    objects = UserManager() 
